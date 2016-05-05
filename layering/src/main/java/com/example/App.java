8a04f1ba73d87aea49aaa38cc0980de2db4b2814
Minseok Lee <lagender@gmail.com>
2016-05-05 16:26:23 +0900

    2.2.1 JdbcTemplate으로 DB 접속하기 - JdbcTemplate을 사용한 객체 매핑

--- layering/src/main/java/com/example/App.java
+++ layering/src/main/java/com/example/App.java
@@ -1,13 +1,19 @@
 package com.example;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+
+import com.example.domain.Customer;
 
 @EnableAutoConfiguration
 @ComponentScan
@@ -17,11 +23,16 @@ public class App implements CommandLineRunner{
 	
 	@Override
 	public void run(String... arg0) throws Exception {
-		String sql = "SELECT :a + :b";
-		SqlParameterSource param = new MapSqlParameterSource()
-				.addValue("a", 100)
-				.addValue("b", 200);
-		Integer result = jdbcTemplate.queryForObject(sql, param, Integer.class);
+		String sql = "SELECT id, first_name, last_name FROM customers WHERE id = :id";
+		SqlParameterSource param = new MapSqlParameterSource().addValue("id", 1);
+		
+		Customer result = jdbcTemplate.queryForObject(sql,param,
+				new RowMapper<Customer>() {
+			public Customer mapRow(ResultSet rs,int rowNum) throws SQLException {
+				return new Customer(rs.getInt("id"), rs.getString("first_name"),rs.getString("last_name"));
+	
+			}
+		});
 		
 		System.out.println("result = "+result);
 		
