482236c480e7ec1ada6a33e22a4d91ee1634dd63
Minseok Lee <lagender@gmail.com>
2016-05-06 16:37:57 +0900

    readme update

--- Readme.md
+++ Readme.md
@@ -1,2 +1,4 @@
 #Springboot study
 * 가장 빨리 만나는 스프링 부트 (SPRING BOOT FOR THE REALLY IMPATIENT) 예제
+---------------------
+기술 스펙 : java8 , springboot, jpa, mysql
--- layering/src/main/java/com/example/App.java
+++ layering/src/main/java/com/example/App.java
@@ -1,14 +1,10 @@
 package com.example;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
