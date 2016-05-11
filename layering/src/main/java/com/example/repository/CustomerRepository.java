84f9ecebb46b1877e81e61c6ee109358d5c52402
Minseok Lee <lagender@gmail.com>
2016-05-11 16:05:02 +0900

    2.2.4 JdbcTemplate으로 리포지토리 클래스 구현하기

--- layering/src/main/java/com/example/repository/CustomerRepository.java
+++ layering/src/main/java/com/example/repository/CustomerRepository.java
@@ -1,33 +1,76 @@
 package com.example.repository;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
+import javax.annotation.PostConstruct;
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 
 import com.example.domain.Customer;
 
 @Repository
+@Transactional
 public class CustomerRepository {
+	@Autowired
+	NamedParameterJdbcTemplate jdbcTemplate;
+	SimpleJdbcInsert insert;
+	
+	private static final RowMapper<Customer> customerRowMapper = (rs, i) -> {
+		Integer id = rs.getInt("id");
+		String firstName = rs.getString("first_name");
+		String lastName = rs.getString("last_name");
+		return new Customer(id, firstName, lastName);
+	};
+	
+	@PostConstruct
+	public void init() {
+		insert = new SimpleJdbcInsert(
+				(JdbcTemplate) jdbcTemplate.getJdbcOperations())
+				.withTableName("customers")
+				.usingGeneratedKeyColumns("id");
+	}
+	
 	private final ConcurrentMap<Integer, Customer> customerMap = 
 			new ConcurrentHashMap<>();
 	
 	public List<Customer> findAll() {
-		return new ArrayList<>(customerMap.values());
+		List<Customer> customers = jdbcTemplate.query(
+				"SELECT id,first_name,last_name FROM customers ORDER BY id",customerRowMapper);
+		return customers;
 	}
 	
-	public Customer findOne(Integer customerId) {
-		return customerMap.get(customerId);
+	public Customer findOne(Integer id) {
+		SqlParameterSource param = new MapSqlParameterSource().addValue("id", id);
+		return jdbcTemplate.queryForObject(
+				"SELECT id,first_name,last_name FROM customers WHERE id=:id",param,customerRowMapper);
 	}
 	
 	public Customer save(Customer customer) {
-		return customerMap.put(customer.getId(), customer);
+		SqlParameterSource param = new BeanPropertySqlParameterSource(customer);
+		if (customer.getId() == null) {
+			Number key = insert.executeAndReturnKey(param);
+			customer.setId(key.intValue());
+		} else {
+			jdbcTemplate.update("UPDATE customers SET first_name=:firstName, "
+					+ "last_name=:lastName WHERE id=:id", param);
+		}
+		return customer;
 	}
 	
-	public void delete(Integer customerId) {
-		customerMap.remove(customerId);
+	public void delete(Integer id) {
+		SqlParameterSource param = new MapSqlParameterSource().addValue("id", id);
+		jdbcTemplate.update("DELETE FROM customers WHERE id=:id", param);
 	}
 	
 }
