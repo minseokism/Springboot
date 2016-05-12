4dbd465de4ca7f7e446ccb4cb6ed340810d9e0c6
Minseok Lee <lagender@gmail.com>
2016-05-12 16:31:45 +0900

    2.3.4 페이징 처리 구현하기

--- layering/src/main/java/com/example/App.java
+++ layering/src/main/java/com/example/App.java
@@ -5,6 +5,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 import com.example.domain.Customer;
 import com.example.repository.CustomerRepository;
@@ -21,8 +24,14 @@ public class App implements CommandLineRunner{
 		Customer created = customerRepository.save(new Customer(
 				null, "Minseok", "Lee"));		
 		System.out.println(created + " is created");
-		customerRepository.findAllOrderByName()
-				.forEach(System.out::println);
+		
+		Pageable pageable = new PageRequest(0, 3);
+		Page<Customer> page = customerRepository.findAllOrderByName(pageable);
+		System.out.println("한 페이지당 데이터 수=" +page.getSize());
+		System.out.println("한 페이지=" +page.getNumber());
+		System.out.println("전체 페이지수=" +page.getTotalPages());
+		System.out.println("전체 데이터수=" +page.getTotalElements());
+		page.getContent().forEach(System.out::println);
 	}
 	
 	public static void main(String[] args) {
