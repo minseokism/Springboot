5003b3b14ef925cae704fb4f638bc40911c013d2
Minseok Lee <lagender@gmail.com>
2016-05-12 15:38:43 +0900

    2.3.2 스프링 데이터 JPA로 리포지토리 클래스 작성하기

--- layering/src/main/resources/data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO customers(first_name, last_name) VALUES('minseok', 'lee');
-INSERT INTO customers(first_name, last_name) VALUES('jihoon', 'lee');
-INSERT INTO customers(first_name, last_name) VALUES('goen', 'kim');
-INSERT INTO customers(first_name, last_name) VALUES('ion', 'lee');
