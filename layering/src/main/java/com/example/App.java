a8554db54ea3645622d1ea41c2d30efaa1c67938
Minseok Lee <lagender@gmail.com>
2016-05-12 16:01:18 +0900

    2.3.3 JPQL로 쿼리 정의하기

--- layering/src/main/java/com/example/App.java
+++ layering/src/main/java/com/example/App.java
@@ -21,7 +21,7 @@ public class App implements CommandLineRunner{
 		Customer created = customerRepository.save(new Customer(
 				null, "Minseok", "Lee"));		
 		System.out.println(created + " is created");
-		customerRepository.findAll()
+		customerRepository.findAllOrderByName()
 				.forEach(System.out::println);
 	}
 	
