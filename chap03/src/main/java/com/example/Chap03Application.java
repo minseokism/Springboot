1059802e594e19ca46075bb285f54e30b28ec52f
Minseok Lee <lagender@gmail.com>
2016-05-13 16:33:49 +0900

    chapter 3

---
+++ chap03/src/main/java/com/example/Chap03Application.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chap03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chap03Application.class, args);
+	}
+}
