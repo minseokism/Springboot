7f95f3faab71c7c51494ad42741542c7fff18312
Minseok Lee <lagender@gmail.com>
2016-05-14 19:50:11 +0900

    3.2.1 모든 고객 정보 얻기, 고객 한 명의 정보 얻기용 API 구현

---
+++ chap03/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO customers(first_name, last_name) VALUES('minseok', 'lee');
+INSERT INTO customers(first_name, last_name) VALUES('jihoon', 'lee');
+INSERT INTO customers(first_name, last_name) VALUES('goen', 'kim');
+INSERT INTO customers(first_name, last_name) VALUES('ion', 'lee');
