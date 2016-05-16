f0867cf67a6f1c7686e4d8f81b1c20a7a58b3c90
Minseok Lee <lagender@gmail.com>
2016-05-16 16:01:24 +0900

    3.2.3 페이징 처리 구현

--- chap03/src/main/java/com/example/service/CustomerService.java
+++ chap03/src/main/java/com/example/service/CustomerService.java
@@ -3,6 +3,8 @@ package com.example.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,6 +17,9 @@ public class CustomerService {
 	@Autowired
 	CustomerRepository customerRepository;
 	
+	public Page<Customer> findAll(Pageable pageable) {
+		return customerRepository.findAllOrderByName(pageable);
+	}
 	public List<Customer> findAll() {
 		return customerRepository.findAll();
 	}
