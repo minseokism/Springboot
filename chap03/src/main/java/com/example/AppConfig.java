86893de81ce6b2934639fb1875c103eda277b7e4
Minseok Lee <lagender@gmail.com>
2016-05-18 17:05:12 +0900

    3.3.1 화면에 고객 정보 목록 표시하기 ( thymeleaf -> freemarker로 대체 )

--- chap03/src/main/java/com/example/AppConfig.java
+++ chap03/src/main/java/com/example/AppConfig.java
@@ -1,14 +1,22 @@
 package com.example;
 
+import java.io.IOException;
+
 import javax.sql.DataSource;
 
-import net.sf.log4jdbc.Log4jdbcProxyDataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.ui.freemarker.FreeMarkerConfigurationFactory;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerViewResolver;
+
+import freemarker.template.TemplateException;
+import net.sf.log4jdbc.Log4jdbcProxyDataSource;
 
 @Configuration
 public class AppConfig {
@@ -32,4 +40,24 @@ public class AppConfig {
 	DataSource dataSource() {
 		return new Log4jdbcProxyDataSource(this.dataSource);
 	}
+	
+    @Bean
+    public ViewResolver viewResolver() {
+        FreeMarkerViewResolver resolver = new FreeMarkerViewResolver();
+        resolver.setCache(true);
+        resolver.setPrefix("");
+        resolver.setSuffix(".ftl");
+        resolver.setContentType("text/html; charset=UTF-8");
+        return resolver;
+    }
+
+    @Bean
+    public FreeMarkerConfigurer freemarkerConfig() throws IOException, TemplateException {
+        FreeMarkerConfigurationFactory factory = new FreeMarkerConfigurationFactory();
+        factory.setTemplateLoaderPaths("classpath:templates", "src/main/resource/templates");
+        factory.setDefaultEncoding("UTF-8");
+        FreeMarkerConfigurer result = new FreeMarkerConfigurer();
+        result.setConfiguration(factory.createConfiguration());
+        return result;
+    }
 }
