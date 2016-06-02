899a3ffe01faf78b596d9bf951758e5af4db94db
Minseok Lee <lagender@gmail.com>
2016-06-02 14:47:03 +0900

    3.5.1 User의 엔티티와 리포지토리 작성하기

--- chap03/src/main/java/com/example/domain/Customer.java
+++ chap03/src/main/java/com/example/domain/Customer.java
@@ -1,6 +1,12 @@
 package com.example.domain;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -15,10 +21,10 @@ public class Customer {
 	@Id
 	@GeneratedValue
 	private Integer id;
-	
-	@Column(nullable = false)
 	private String firstName;
-	
-	@Column(nullable = false)
 	private String lastName;
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(nullable = true, name = "username")
+	private User user;
+	
 }
